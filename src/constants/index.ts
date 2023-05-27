622f5f1dd36578fbbd8a06da28e275b79be7dbcd
illusion77 <illusioncyk@gmail.com>
2023-05-27 16:16:48 -0700

    fix: set user_agent platform to android

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,7 @@ export const LOGIN_SUCCESS_PAGE = `${HOST_URL}/api/auth/callback`;
 // The user agent is obtained from https://user-agents.net/string/mozilla-5-0-iphone-cpu-iphone-os-10-3-like-mac-os-x-applewebkit-602-1-50-khtml-like-gecko-crios-90-0-2924-75-mobile-14e5239e-safari-602-1
 // https://github.com/react-native-webview/react-native-webview/issues/162
 export const USER_AGENT =
-  Platform.OS === 'ios'
+  Platform.OS === 'ios' && false
     ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/90.0.2924.75 Mobile/14E5239e Safari/602.1'
     : 'Mozilla/5.0 (Linux; Android 13; SM-N981B Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/108.0.5359.128 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/392.0.0.12.106;]';
 
